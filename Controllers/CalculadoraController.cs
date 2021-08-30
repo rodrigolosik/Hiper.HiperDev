28b3c839cf79a7d20d9b4d4b3f7c1f98eea73fd2
Rodrigo Losik <rodrigolosik@gmail.com>
2021-08-30 19:52:25 -0300

    Adicionadas implementações das operações matemáticas

--- Controllers/CalculadoraController.cs
+++ Controllers/CalculadoraController.cs
@@ -14,18 +14,18 @@ namespace Hiper.HiperDev.Git.Controllers
 
         [HttpGet("somar/{n1:double}/{n2:double}")]
         public IActionResult Somar(double n1, double n2) =>
-            Ok(_calculadoraServices.Somar(n1, n2));
+            Ok($"{n1} + {n2} = {_calculadoraServices.Somar(n1, n2)}");
 
         [HttpGet("subtrair/{n1:double}/{n2:double}")]
         public IActionResult Subtrair(double n1, double n2) =>
-            Ok(_calculadoraServices.Subtrair(n1, n2));
+            Ok($"{n1} - {n2} = {_calculadoraServices.Subtrair(n1, n2)}");
 
         [HttpGet("dividir/{n1:double}/{n2:double}")]
         public IActionResult Dividir(double n1, double n2) =>
-            Ok(_calculadoraServices.Dividir(n1, n2));
+            Ok($"{n1} / {n2} = {_calculadoraServices.Dividir(n1, n2)}");
 
         [HttpGet("multiplicar/{n1:double}/{n2:double}")]
         public IActionResult Multiplicar(double n1, double n2) =>
-            Ok(_calculadoraServices.Multiplicar(n1, n2));
+            Ok($"{n1} * {n2} = {_calculadoraServices.Multiplicar(n1, n2)}");
     }
 }
--- Services/CalculadoraServices.cs
+++ Services/CalculadoraServices.cs
@@ -1,27 +1,13 @@
-﻿using System;
-
-namespace Hiper.HiperDev.Git.Services
+﻿namespace Hiper.HiperDev.Git.Services
 {
     public class CalculadoraServices : ICalculadoraServices
     {
-        public double Dividir(double n1, double n2)
-        {
-            throw new NotImplementedException();
-        }
+        public double Dividir(double n1, double n2) => n1 / n2;
 
-        public double Multiplicar(double n1, double n2)
-        {
-            throw new NotImplementedException();
-        }
+        public double Multiplicar(double n1, double n2) => n1 * n2;
 
-        public double Somar(double n1, double n2)
-        {
-            throw new NotImplementedException();
-        }
+        public double Somar(double n1, double n2) => n1 + n2;
 
-        public double Subtrair(double n1, double n2)
-        {
-            throw new NotImplementedException();
-        }
+        public double Subtrair(double n1, double n2) => n1 - n2;
     }
 }
